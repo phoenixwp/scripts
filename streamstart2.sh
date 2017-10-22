e3b0e0ed9885576b23152cab3921d37550ecdf81
phoenixwp <renegade.wp@gmail.com>
2017-10-22 22:48:50 +0300

    Update streamstart2.sh
    Create and start single streamer container

--- streamstart2.sh
+++ streamstart2.sh
@@ -1,20 +1,15 @@
+
 #!/bin/bash
-#for i in `seq 1 4`;
-#do
-let http_port=$1+8080
-let https_port=$1+8400
-
-if [ "$1" -ge 10 ]
-then 
-        id=$1
-        echo $id
-else
-        id=0$1
-        echo $id
-fi
-
-docker create --name restreamer-$id --restart always -e "RESTREAMER_USERNAME=admin" -e "RESTREAMER_PASSWORD=4820919" -e "LOGGER_LEVEL=4" -e "TIMEZONE=Europe/Kiev" -p $http_port:8080 -p $https_port:443 -v /mnt/restreamer/db$id:/restreamer/db -v /ssl:/ssl test-ms-streamer-slim
-#docker start restreamer-$id
-#done
+i=$1
+	let http_port=$i+8080
+	let https_port=$i+8400
+	if [ "$i" -ge 10 ]
+	then
+            id=$i
+	else
+            id=0$i
+	fi
+	docker create --name restreamer-$id --restart always -e "RESTREAMER_USERNAME=admin" -e "RESTREAMER_PASSWORD=4820919" -e "LOGGER_LEVEL=4" -e "TIMEZONE=Europe/Kiev" -p $http_port:8080 -p $https_port:443 -v /mnt/restreamer/db$id:/restreamer/db -v /ssl:/ssl ms-streamer
+	docker start restreamer-$id
 docker ps
 exit
